4f9a380e9b96870b6087579a9f3b20af1776c872
GorokhovG <gorokhov.gw@gmail.com>
2025-06-20 12:32:30 +0200

    Added Splash Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,16 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.kiat.briCardGame"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.kiat.briCardGame"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -35,6 +36,19 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.core.ktx)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.firestore)
+
+//    implementation(libs.lottie)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+
+    implementation(libs.lottie)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/com/kiat/briCardGame/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.kiat.briCardGame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 2000; // 2 секунды
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Можно запускать анимацию через LottieAnimationView, если надо программно
+
+        new Handler().postDelayed(() -> {
+            // Переход в MenuActivity
+            startActivity(new Intent(SplashActivity.this, MenuActivity.class));
+            finish();
+        }, SPLASH_DELAY);
+    }
+}

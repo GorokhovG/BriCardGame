92cca899c9f4944a7c08ee8e495e79b45e9c8dee
GorokhovG <gorokhov.gw@gmail.com>
2025-06-20 12:45:42 +0200

    Removed Action Bar

--- app/src/main/java/com/kiat/briCardGame/SplashActivity.java
+++ app/src/main/java/com/kiat/briCardGame/SplashActivity.java
@@ -1,8 +1,11 @@
 package com.kiat.briCardGame;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -12,6 +15,8 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
 
         // Можно запускать анимацию через LottieAnimationView, если надо программно
